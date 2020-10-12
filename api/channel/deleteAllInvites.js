6bd6ea1205b8234b7bc2f8facef73b16b5c27f3b
Michael Nalbandyan <xinger@ya.ru>
2020-10-12 20:03:51 +0300

    fixes

--- api/channel/deleteAllInvites.js
+++ api/channel/deleteAllInvites.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 /**
  * Create invite token to specific channel
  * @param {string} id - id of channel
+ * @param {boolean} revokeAccess – revoke users access in this channel
  * @returns {string} 'ok'
  */
 export default async function (id, revokeAccess = false) {
