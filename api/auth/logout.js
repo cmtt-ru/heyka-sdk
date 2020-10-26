85b10122fe8b79b4811b8c3832f3fc43800b9d2c
ivan <bushmin@list.ru>
2020-10-26 15:27:37 +0300

    logout

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -2,6 +2,7 @@ import { clearTokens } from '../tokens';
 import router from '@/router';
 import * as sockets from '@api/socket';
 import broadcastEvents from '@sdk/classes/broadcastEvents';
+import broadcastActions from '@sdk/classes/broadcastActions';
 
 /** List for event and logout user */
 broadcastEvents.on('logout', logout);
@@ -15,9 +16,11 @@ broadcastEvents.on('logout', logout);
 export default function logout(redirectToAuth = true) {
   clearTokens();
 
+  broadcastActions.dispatch('unselectChannelWithoutAPICall');
+
   sockets.destroy();
 
   if (redirectToAuth) {
-    router.replace({ name: 'auth' });
+    router.replace({ name: 'auth' }).catch(() => {});
   }
 }
