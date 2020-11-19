1abe5176022938f7b2c3e8de15966440c711582c
Michael Nalbandyan <xinger@ya.ru>
2020-11-19 21:32:43 +0300

    auto refresh & navigation

--- api/admin/getWorkpsaceStateWithJanusStats.js
+++ api/admin/getWorkpsaceStateWithJanusStats.js
@@ -3,10 +3,17 @@ import axios from 'axios';
 /**
  * Get full workspace state with janus stats
  * @param {string} workspaceId – workspace id
+ * @param {Date} before — filter by before date
+ * @param {number} count – state's count
  * @returns {object}
  */
-export default async function (workspaceId) {
-  const res = await axios.get(`/admin/workspaces/${workspaceId}/janus-stats`);
+export default async function (workspaceId, before, count = 1) {
+  const res = await axios.get(`/admin/workspaces/${workspaceId}/janus-stats`, {
+    params: {
+      before,
+      count,
+    },
+  });
 
   return res.data;
 }
