d421d111d0e99810ffe884dfd7b8c33dd60750d1
Michael Nalbandyan <xinger@ya.ru>
2021-02-25 18:44:06 +0300

    OMG PseudoPopup

--- components/PseudoPopup.vue
+++ components/PseudoPopup.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="pseudo-popup">
     <div
-      v-sticky="{ offset: 0, rootSelector: '.layout' }"
+      v-if="$slots.header"
       class="pseudo-popup__header"
-      :class="{'shadow-disable': !headerHasShadow}"
+      :class="{'pseudo-popup__header--with-shadow': headerShadow}"
     >
       <span>
         <slot name="header" />
@@ -19,25 +19,56 @@
     </div>
 
     <div
+      v-if="$slots['custom-header']"
+      class="pseudo-popup__header pseudo-popup__header--custom"
+      :class="{'pseudo-popup__header--with-shadow': headerShadow}"
+    >
+      <slot name="custom-header" />
+    </div>
+
+    <div
+      v-if="$slots.body"
       ref="body"
-      class="pseudo-popup__body"
+      class="pseudo-popup__body scroll"
     >
       <slot name="body" />
     </div>
 
     <div
+      v-if="$slots['custom-body']"
+      ref="body"
+      class="pseudo-popup__body pseudo-popup__body--custom scroll"
+    >
+      <slot name="custom-body" />
+    </div>
+
+    <div
       v-if="$slots.footer"
-      v-sticky.bottom="{ offset: 0, rootSelector: '.layout' }"
       class="pseudo-popup__footer"
-      :class="{'shadow-disable': !footerHasShadow}"
+      :class="{'pseudo-popup__footer--with-shadow': footerShadow}"
     >
       <slot name="footer" />
+    </div>
+
+    <div
+      v-if="$slots['custom-footer']"
+      class="pseudo-popup__footer pseudo-popup__footer--custom"
+      :class="{'pseudo-popup__footer--with-shadow': footerShadow}"
+    >
+      <slot name="custom-footer" />
     </div>
   </div>
 </template>
 
 <script>
 import UiButton from '@components/UiButton';
+import { throttle } from 'throttle-debounce';
+
+/**
+ * Scroll throttle timeout
+ * @type {number}
+ */
+const THROTTLE_TIMEOUT = 200;
 
 export default {
   components: {
@@ -62,7 +93,51 @@ export default {
     },
   },
 
+  data() {
+    return {
+      headerShadow: false,
+      footerShadow: false,
+    };
+  },
+
+  updated() {
+    this.scrollHandler();
+  },
+
+  mounted() {
+    this.$refs.body.addEventListener('scroll', this.scrollHandler);
+    window.addEventListener('resize', this.scrollHandler);
+
+    this.$nextTick(() => {
+      this.scrollHandler();
+    });
+  },
+
+  beforeDestroy() {
+    this.$refs.body.removeEventListener('scroll', this.scrollHandler);
+    window.removeEventListener('resize', this.scrollHandler);
+  },
+
   methods: {
+    /**
+     * Handle scroll event on body slot
+     * @returns {void}
+     */
+    scrollHandler: throttle(THROTTLE_TIMEOUT, function () {
+      if (this.$refs.body.scrollHeight > this.$refs.body.clientHeight) {
+        if (this.headerHasShadow) {
+          this.headerShadow = this.$refs.body.scrollTop > 0;
+        }
+
+        if (this.footerHasShadow) {
+          this.footerShadow = this.$refs.body.clientHeight < this.$refs.body.scrollHeight - this.$refs.body.scrollTop;
+        }
+      } else {
+        this.headerShadow = false;
+        this.footerShadow = false;
+      }
+    }),
+
     /**
      * Close handler
      * @returns {void}
@@ -78,7 +153,7 @@ export default {
   .pseudo-popup
     display flex
     flex-direction column
-    min-height 100%
+    height 100%
 
     &__header
       display flex
@@ -88,32 +163,39 @@ export default {
       font-weight 500
       background var(--new-bg-04)
       padding 6px 15px 0 16px
-      box-shadow none
       transition box-shadow 0.15s ease
-      z-index 10
-
-      &.ui-sticked:not(.shadow-disable)
-        box-shadow 0 0 0 1px var(--new-UI-06)
 
       &__close
         margin-left auto
+
+      &--custom
+        flex 0 0 auto
+        padding 0
+
+      &--with-shadow
+        box-shadow 0 0 0 1px var(--shadow-10)
 
     &__body
       flex 1 1 auto
       padding 0 16px
 
+      &--custom
+        padding 0
+
     &__footer
       display flex
       flex 0 0 56px
       background var(--new-bg-04)
-      padding 12px 16px
+      padding 16px
       box-sizing border-box
       transition box-shadow 0.15s ease
       flex-direction row-reverse
-      box-shadow none
-      z-index 10
 
-      &.ui-sticked:not(.shadow-disable)
-          box-shadow 0 0 0 1px var(--new-UI-06)
+      &--custom
+        flex 0 0 auto
+        padding 0
+
+      &--with-shadow
+        box-shadow 0 0 0 1px var(--shadow-10)
 
 </style>
