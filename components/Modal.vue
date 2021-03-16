4f3f301b1871d017099582b25f2a54555ccf114f
Michael Nalbandyan <xinger@ya.ru>
2021-03-16 20:05:24 +0300

    lint fix

--- components/Modal.vue
+++ components/Modal.vue
@@ -9,22 +9,23 @@
       class="modal__backdrop"
       @click="$emit('close')"
     >
-      <slot name="backdrop"/>
+      <slot name="backdrop" />
     </div>
-    <slot/>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    // eslint-disable-next-line strict-vue/require-jsdoc
     centered: {
       default: true,
       type: Boolean,
     },
   },
 
-   data() {
+  data() {
     return {
       scrollPosition: null,
     };
@@ -33,18 +34,21 @@ export default {
   mounted() {
     const close = (e) => {
       const ESC = 27;
-      if (e.keyCode !== ESC) return;
+
+      if (e.keyCode !== ESC) {
+        return;
+      }
       this.$emit('close');
     };
+
     // Close the modal when the
     // user presses the ESC key.
     document.addEventListener('keyup', close);
     // Activate the modal when the component is mounted.
     this.activate();
-
   },
 
-  destroyed(){
+  destroyed() {
     document.removeEventListener('keyup', close);
     this.deactivate();
   },
@@ -64,21 +68,25 @@ export default {
       this.scrollPosition = window.pageYOffset;
 
       const body = document.querySelector('body');
+
       body.style.overflowY = 'scroll';
       body.style.position = 'fixed';
       body.style.top = `-${this.scrollPosition}px`;
       body.style.width = '100%';
     },
 
-
     enableScrolling() {
       const body = document.querySelector('body');
+
       body.style.removeProperty('overflowY');
       body.style.removeProperty('position');
       body.style.removeProperty('top');
       body.style.removeProperty('width');
 
-      window.scrollTo({ top: this.scrollPosition, behavior: "instant"});
+      window.scrollTo({
+        top: this.scrollPosition,
+        behavior: 'instant',
+      });
     },
 
   },
@@ -87,21 +95,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.modal 
+.modal
   position fixed
   top 0
   right 0
   bottom 0
   left 0
 
-
-.modal--centered 
+.modal--centered
   display flex
   justify-content center
   align-items center
 
-
-.modal__backdrop 
+.modal__backdrop
   position absolute
   width 100%
   height 100%
