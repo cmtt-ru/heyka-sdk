1102cb6798a26f1009d8487847ba72ee037e4b06
Иван <bushmin@list.ru>
2021-03-19 19:11:21 +0300

    modals 1.0

--- classes/Modal.js
+++ classes/Modal.js
@@ -1,279 +1,35 @@
-import Vue from 'vue';
-import router from '@/router';
+
 import store from '@/store';
-import i18n from '@sdk/translations/i18n';
-import { createPopper } from '@popperjs/core';
-import Permissions from '@sdk/classes/permissions';
-
 
 /**
- * Max number for unique id
- * @type {number}
+ * Singleton Modal
  */
-const UID_MAX = 1000000;
-
-/**
- * Wait before destroy popover
- * @type {number}
- */
-const TIMEOUT_BEFORE_DESTROY = 70;
-
-
-/**
- * Disable hover delay for fast moving cursor
- * @type {boolean}
- */
-let disableHoverDelay = false;
-
-/**
- * Class for popovers
- */
-export default class Modal {
-  /**
-   * Modal constructor
-   * @param {object} options – popover options
-   * @param {string} componentName – context menu component name
-   * @param {object} data – some data to pass to context menu component
-   */
-  constructor({ element, options = {}, componentName, data = {}, permissions = null }) {
-    this.uid = Math.round(Math.random() * UID_MAX);
-
-    this.options = options;
-    this.componentName = componentName;
-    this.vueProps = data;
-    this.permissions = permissions;
-
-    this.instance = null;
-    this.popper = null;
-
-    this.showInProgress = false;
-
-    this.mouseCoordinates = {
-      x: 0,
-      y: 0,
-    };
-
-    this.showState = false;
-
-    this.bind();
+ class Modal {
+  constructor() {
+    this.modalComponent = null;
+    this.onCloseCallback = null;
   }
-
-  /**
-   * Create virtual element with specific bounding client rect
-   *
-   * @param {HTMLElement} element – dom element
-   * @param {number} x – x coordinate
-   * @param {number} y – y coordinate
-   * @returns {object}
-   */
-  createVirtualElement(element, x = 0, y = 0) {
-    return {
-      getBoundingClientRect: () => ({
-        width: 0,
-        height: 0,
-        top: y,
-        right: x,
-        bottom: y,
-        left: x,
-      }),
-      contextElement: element,
-    };
+  show({ name, data, onClose }) {
+    /* ... */
+    store.dispatch('app/addModal', { name, data });
+    this.onCloseCallback = onClose;
   }
-
-  /**
-   * Dynamically load vue component
-   *
-   * @param {string} name – path to component
-   * @returns {object}
-   */
-  async loadComponent(name) {
-    try {
-      const component = await import(/* webpackMode: "eager" */ `@views/Modals/${name}.vue`);
-
-      if (component.default) {
-        return component.default;
-      }
-    } catch (err) {
-      const component = await import(/* webpackMode: "eager" */ `@sdk/views/Modals/${name}.vue`);
-
-      if (component.default) {
-        return component.default;
-      }
-    }
+  setModalComponent(vueComponent) {
+    this.modalComponent = vueComponent;
+    console.log(this.modalComponent);
+    this.modalComponent.$on('confirm', () => { this.vueEventHandler('confirm')});
+    this.modalComponent.$on('reject', () => { this.vueEventHandler('reject')});
+    this.modalComponent.$on('close', () => { this.vueEventHandler('close')});
   }
-
-  /**
-   * Mount vue component
-   *
-   * @returns {Promise<void>}
-   */
-  async mount() {
-    if (this.instance) {
-      return;
-    }
-
-    if (this.permissions) {
-      this.vueProps.permissions = await Permissions.canI(this.permissions) || {};
-    }
-
-    const Component = await this.loadComponent(this.componentName);
-    const ComponentClass = Vue.extend(Component);
-
-    ComponentClass.options.router = router;
-    ComponentClass.options.store = store;
-    ComponentClass.options.i18n = i18n;
-
-    this.instance = new ComponentClass({
-      propsData: this.vueProps,
-    });
-
-    this.instance.$mount();
-
-    document.body.appendChild(this.instance.$el);
-  }
-
-  /**
-   * Unmount and destroy vue component
-   *
-   * @returns {void}
-   */
-  unmount() {
-    if (this.instance) {
-      this.instance.$destroy();
-      this.instance.$el.remove();
-      this.instance = null;
-    }
-  }
-
-  /**
-   * Bind events on element
-   *
-   * @returns {void}
-   */
-  bind() {
-    /* Handle click mode */
-    if (this.modes.click) {
-      this.element.__clickHandler = event => {
-        if (this.modes.mouse) {
-          this.options = {};
-          this.options.placement = 'bottom-start';
-
-          this.mouseCoordinates = {
-            x: event.pageX,
-            y: event.pageY,
-          };
-
-          if (this.modes.right) {
-            if (event.which === RIGHT_MOUSE) {
-              this.show(true);
-            }
-          } else {
-            if (event.which === LEFT_MOUSE) {
-              this.show(true);
-            }
-          }
-        } else {
-          this.show(!this.showState);
-        }
-      };
-
-      this.element.__clickOutsideHandler = event => {
-        const conditionToHide =
-          (!(this.instance.$el === event.target || this.instance.$el.contains(event.target)) && !this.options.disableOutsideClick) ||
-          (event.target.hasAttribute('data-popover-close') || event.target.closest('[data-popover-close]'));
-
-        if (conditionToHide) {
-          setTimeout(() => {
-            this.show(false);
-          }, TIMEOUT_BEFORE_DESTROY);
-        }
-      };
-
-      this.element.addEventListener('mouseup', this.element.__clickHandler);
-    }
-
-    /* Handle hover mode */
-    if (this.modes.hover) {
-      this.element.__mouseEnterHandler = event => {
-        clearTimeout(this.element.__hoverInterval);
-
-        this.element.__hoverInterval = setTimeout(() => {
-          this.show(true);
-
-          disableHoverDelay = true;
-        }, !disableHoverDelay && HOVER_DELAY);
-      };
-
-      this.element.__mouseLeaveHandler = event => {
-        clearTimeout(this.element.__hoverInterval);
-
-        this.element.__hoverInterval = setTimeout(() => {
-          disableHoverDelay = false;
-        }, HOVER_DELAY);
-
-        this.show(false);
-      };
-
-      this.element.addEventListener('mouseenter', this.element.__mouseEnterHandler);
-      this.element.addEventListener('mouseleave', this.element.__mouseLeaveHandler);
-    }
-  }
-
-
-  /**
-   * Show popover
-   *
-   * @param {boolean} state – whether to show or hide
-   * @returns {Promise<void>}
-   */
-  async show(state = true) {
-    if (this.showInProgress) {
-      return;
-    }
-
-    this.showInProgress = true;
-
-    if (state) {
-      await this.mount();
-
-      let refElement = this.element;
-
-      if (this.modes.mouse) {
-        refElement = this.createVirtualElement(this.element, this.mouseCoordinates.x, this.mouseCoordinates.y);
-      }
-
-      this.popper = createPopper(refElement, this.instance.$el, this.options);
-
-      refElement = null;
-      setTimeout(() => {
-        this.popper.update();
-        document.body.addEventListener('mouseup', this.element.__clickOutsideHandler);
-      }, 0);
-      this.showState = true;
+  vueEventHandler(event) {
+    console.log('vueEventHandler', event)
+    if (event === 'confirm') {
+      this.onCloseCallback(true);
     } else {
-      await this.unmount();
-      if (this.element && this.element.__clickOutsideHandler) {
-        document.body.removeEventListener('mouseup', this.element.__clickOutsideHandler);
-      }
-      if (this.popper) {
-        this.popper.destroy();
-        this.popper = null;
-      }
-      this.showState = false;
+      this.onCloseCallback(false);
     }
-
-    this.showInProgress = false;
-  }
-
-
-  /**
-   * Destroy popover instance
-   *
-   * @returns {void}
-   */
-  destroy() {
-    this.unmount();
-    this.instance = null;
-    this.vueProps = null;
+    store.dispatch('app/removeModal');
   }
 }
+
+export default new Modal()
