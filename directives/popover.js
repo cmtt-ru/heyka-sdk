87b3f1c992ed41c8679bbfbed1b4242338afde72
Иван <bushmin@list.ru>
2021-04-05 12:52:49 +0300

    popover update fix

--- directives/popover.js
+++ directives/popover.js
@@ -16,6 +16,34 @@ export default {
     el.setAttribute('popover-instance-uid', popover.uid);
   },
 
+  update: (el, binding, vnode) => {
+    if (JSON.stringify(binding.oldValue.data) === JSON.stringify(binding.value.data)) {
+      return;
+    }
+
+    /** trigger this.unbind() */
+    const popoverUid = el.getAttribute('popover-instance-uid');
+    const oldPopover = vnode.context[`_popoverInstance${popoverUid}`];
+
+    oldPopover.destroy();
+
+    delete vnode.context[`_popoverInstance${popoverUid}`];
+
+    /** trigger this.bind() */
+
+    const newPopover = new Popover({
+      element: el,
+      componentName: binding.value.name,
+      options: binding.value.options,
+      data: binding.value.data,
+      modes: binding.modifiers,
+      permissions: binding.value.permissions,
+    });
+
+    vnode.context[`_popoverInstance${newPopover.uid}`] = newPopover;
+    el.setAttribute('popover-instance-uid', newPopover.uid);
+  },
+
   unbind: (el, binding, vnode) => {
     /** Get popover instance from vnode context by his uid */
     const popoverUid = el.getAttribute('popover-instance-uid');
