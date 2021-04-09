f09fa7e8acebe0354eaf7845f6142199d2181744
Иван <bushmin@list.ru>
2021-04-09 15:23:48 +0300

    audit first wave

--- components/ChannelItem.vue
+++ components/ChannelItem.vue
@@ -226,8 +226,8 @@ export default {
   background-color var(--new-UI-07)
 
 .channel
-  padding 4px 0
-  margin 2px 0
+  padding 6px 0
+  margin 4px 0
   width 100%
   border-radius 6px
   display flex
@@ -269,7 +269,7 @@ export default {
 
   &__more
     color var(--new-UI-04)
-    margin 0 4px
+    margin 0 6px
     flex-shrink 0
     visibility hidden
 
