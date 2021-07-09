60b321208567f1791c4e110bf0cdeec859e809f8
Michael Nalbandyan <xinger@ya.ru>
2021-07-09 18:58:21 +0300

    zoom to cursor alpha

--- components/ZoomPan.vue
+++ components/ZoomPan.vue
@@ -3,6 +3,7 @@
     ref="root"
     class="zoom-pan"
     @wheel="wheelHandler"
+    @mousemove="mouseMoveHandler"
   >
     <div
       ref="container"
@@ -10,6 +11,7 @@
     >
       <slot />
     </div>
+    <div class="dot" />
   </div>
 </template>
 
@@ -29,16 +31,15 @@ export default {
       containerX: 0,
       containerY: 0,
       containerScale: 1,
+
+      mouseX: 0,
+      mouseY: 0,
     };
   },
 
   async mounted() {
     this.resizeObserver = new ResizeObserver(this.updateSizes);
     this.resizeObserver.observe(this.$refs.root);
-
-    this.$refs.root.addEventListener('gotpointercapture', () => {
-      console.log('asdasdasdasdasasda');
-    });
   },
 
   beforeDestroy() {
@@ -54,21 +55,23 @@ export default {
     wheelHandler(e) {
       e.preventDefault();
 
+      // console.log(this.mouseX / this.rootWidth);
+
       if (e.ctrlKey) {
         const i = Math.exp(-e.deltaY / WHEEL_K);
 
         this.containerScale *= i;
-        // (elem.offsetWidth / 2 - offsetX);
-        console.log(this.rootWidth / 2 - e.offsetX);
-        const { width, height } = this.$refs.root.getBoundingClientRect();
 
-        console.log('----CX', i * (width / 2 - e.offsetX));
-        console.log('----CY', i * (height / 2 - e.offsetY));
+        // eslint-disable-next-line no-unused-vars
+        // const { width, height, x, y } = this.$refs.root.getBoundingClientRect();
 
-        // eslint-disable-next-line no-magic-numbers
-        this.containerX = i * (this.rootWidth / 2 - e.offsetX);
-        // eslint-disable-next-line no-magic-numbers
-        // this.containerY += i * (height / 2 - e.offsetY);
+        if (this.containerScale < MAX_ZOOM && this.containerScale > MIN_ZOOM) {
+          // this.containerX = (this.containerScale - 1) * (this.rootWidth / 2 - this.mouseX);
+          // this.containerY = (this.containerScale - 1) * (this.rootHeight / 2 - this.mouseY);
+
+          this.containerX += ((i - 1) * (((this.rootWidth / 2 - this.mouseX) * this.containerScale) - this.containerX));
+          this.containerY += ((i - 1) * (((this.rootHeight / 2 - this.mouseY) * this.containerScale) - this.containerY));
+        }
       } else {
         this.containerX += e.deltaX * DIRECTION;
         this.containerY += e.deltaY * DIRECTION;
@@ -87,6 +90,7 @@ export default {
        * Restrict x axis
        */
       const maxX = (this.rootWidth * this.containerScale - this.rootWidth) / 2;
+      // const maxX = (this.containerScale - 1) * e.offsetX;
 
       if (this.containerX <= -maxX) {
         this.containerX = -maxX;
@@ -98,6 +102,7 @@ export default {
        * Restrict y axis
        */
       const maxY = (this.rootHeight * this.containerScale - this.rootHeight) / 2;
+      // const maxY = (this.containerScale - 1) * e.offsetY;
 
       if (this.containerY <= -maxY) {
         this.containerY = -maxY;
@@ -106,6 +111,11 @@ export default {
       }
 
       this.$refs.container.style.transform = 'translate(' + this.containerX + 'px, ' + this.containerY + 'px) scale(' + this.containerScale + ')';
+    },
+
+    mouseMoveHandler(e) {
+      this.mouseX = e.offsetX;
+      this.mouseY = e.offsetY;
     },
 
     /**
@@ -131,4 +141,16 @@ export default {
       position absolute
       width 100%
       height 100%
+
+    .dot
+      position absolute
+      left 50%
+      top 50%
+      width 4px
+      height 4px
+      margin -2px 0 0 -2px
+      border-radius 100%
+      background red
+      opacity 0.5
+      pointer-events none
 </style>
